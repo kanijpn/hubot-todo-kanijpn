df6fcffc8caabdd0c5f79a145ee79d742ce9f737
kanijpn <masahiro@okatani.com>
2016-10-22 09:12:20 +0900

    Herokuでhubotconstletvarアロー

--- scripts/todo.js
+++ scripts/todo.js
@@ -7,10 +7,8 @@
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
-var todo;
-var task;
 var todo = require('todo');
-module.exports = (robot) => {
+module.exports = function (robot) {
     robot.respond(/todo (.+)/i, (msg) => {
         var task = msg.match[1].trim();
         todo.todo(task);
